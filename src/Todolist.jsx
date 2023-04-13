77e0e0965a8cc14d1b451827bda05a242510ef52
Minouccc <343723899@qq.com>
2023-04-13 09:46:22 +0800

    feat: add addTask and deleteTask

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -2,20 +2,34 @@ import React, { memo, useState } from 'react'
 
 const Todolist = memo(() => {
   const [newtask,setNewTask] = useState("")
+  const [todolist,setTodolist] = useState([])  
 
   const handleChange = (event) => {
     setNewTask(event.target.value)
   }
-
-
+  const addTask = () => {
+    const newTodolist = [...todolist,newtask]
+    setTodolist(newTodolist)
+  }
+  const deleteTask = (taskName) => {
+    const newTodolist = todolist.filter(task => task !== taskName)
+    setTodolist(newTodolist)
+  }
   return (
     <div className="to-do-list">
         <div className="add-task">
             <input type="text" onChange={handleChange}/>
-            <button className="add-btn">Add Task</button>
+            <button className="add-btn" onClick={addTask}>Add Task</button>
         </div>
-        <div className="task-list">
-            {newtask}
+        <div className="content">
+            {todolist.map((task,index) => {
+                return(
+                    <div className='task-list' key={index}>
+                        <div className="task">{task}</div>
+                        <button className='delete-btn' onClick={()=> deleteTask(task)}>delete</button>
+                    </div>
+                )
+            })}
         </div>
     </div>
   )
